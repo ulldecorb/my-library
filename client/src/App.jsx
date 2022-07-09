11897007a40cabcfd057d697ccedb9321c043a1d
ulldecorb <ausias.bertran@gmail.com>
2022-07-09 20:38:54 +0200

    Create navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { NavBar } from './components/navbar';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ client/src/components/navbar/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './navbar.css';
+
+export function NavBar() {
+  return (
+    <navbar className="navbar">
+      <h1>My Library</h1>
+    </navbar>
+  );
+}
+
+export default NavBar;
