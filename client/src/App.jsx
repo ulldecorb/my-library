9cb85a614f8580a44495f74c5c9282c6f127e8d3
ulldecorb <ausias.bertran@gmail.com>
2022-07-11 01:27:08 +0200

    Create Gallery component Connect props Create library usestate Create useeffect on app to fetch data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavBar } from './components/navbar';
+import { Gallery } from './components/gallery';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [library, setLibrary] = useState([]);
+
+  useEffect(() => {
+    fetch('/api')
+      .then((res) => res.json())
+      .then((data) => setLibrary(data));
+  });
+
   return (
     <div className="App">
       <NavBar />
+      <Gallery library={library} />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
